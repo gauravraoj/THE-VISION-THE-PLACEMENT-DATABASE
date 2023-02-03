7ec719da3039a633e690a0711aa7c1de92b3b963
gauravraoj <96016132+gauravraoj@users.noreply.github.com>
2023-02-03 07:17:55 -0800

    Add files via upload

---
+++ DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author DELL
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306placement, String root, String gaurav29) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
